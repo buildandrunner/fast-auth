2251f8343d8d4b353b1ae879df499b62b0e93c87
Cesar <cesar.marcial@cabrasportshq.com>
2025-01-27 19:39:23 -0800

    Expanded core interfaces

--- internal/core/port/auth.go
+++ internal/core/port/auth.go
@@ -7,6 +7,7 @@ import (
 	"github.com/mar-cial/space-auth/internal/core/domain"
 )
 
+// auth core
 type AuthHandler interface {
 	Register(ctx *gin.Context)
 	Login(ctx *gin.Context)
@@ -14,9 +15,8 @@ type AuthHandler interface {
 }
 
 type AuthService interface {
-	Register(ctx context.Context, phonenumber string) (*domain.User, error)
-	Login(ctx context.Context, phonenumber string) (*domain.User, error)
-	Logout(ctx context.Context, token string) error
+	UserService
+	SessionService
 }
 
 type AuthRepository interface {
@@ -24,13 +24,32 @@ type AuthRepository interface {
 	SessionRepository
 }
 
+// service layer
+type UserService interface {
+	CreateUser(ctx context.Context, creds domain.Credentials) (*domain.User, error)
+	ReadUserById(ctx context.Context, id string) (*domain.User, error)
+	ReadUserByPhone(ctx context.Context, phonenumber string) (*domain.User, error)
+	UpdateUser(ctx context.Context, user domain.User) (*domain.User, error)
+	DeleteUser(ctx context.Context, id string) error
+}
+
+type SessionService interface {
+	CreateSession(ctx context.Context, userid string) (*domain.Session, error)
+	ReadSession(ctx context.Context, token string) (*domain.Session, error)
+	DeleteSession(ctx context.Context, token string) error
+}
+
+// repo layer
 type UserRepository interface {
-	SaveUser(ctx context.Context, user *domain.User) error
-	FindUserByPhone(ctx context.Context, phonenumber string) (*domain.User, error)
+	SaveUser(ctx context.Context, user domain.User) (string, error)
+	ReadUserByID(ctx context.Context, id string) (*domain.User, error)
+	ReadUserByPhone(ctx context.Context, phone string) (*domain.User, error)
+	UpdateUser(ctx context.Context, user domain.User) (*domain.User, error)
+	DeleteUser(ctx context.Context, user domain.User) error
 }
 
 type SessionRepository interface {
-	CreateSession(ctx context.Context, session *domain.Session) error
+	SaveSession(ctx context.Context, session domain.Session, userid string) (string, error)
 	FindSessionByToken(ctx context.Context, token string) (*domain.Session, error)
 	DeleteSession(ctx context.Context, token string) error
 }
