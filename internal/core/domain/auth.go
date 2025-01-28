02f6b9b7f6d1c1d9c36a40004f37f23239cfd2ea
Cesar <cesar.marcial@cabrasportshq.com>
2025-01-27 19:37:01 -0800

    More robust models

--- internal/core/domain/auth.go
+++ internal/core/domain/auth.go
@@ -1,15 +1,24 @@
 package domain
 
-import "time"
+import (
+	"time"
+)
 
 type User struct {
 	ID          string `json:"id"`
 	Phonenumber string `json:"phonenumber"`
+	Password    string `json:"-"`
 }
 
 type Session struct {
 	ID        string    `json:"id"`
 	Token     string    `json:"token"`
+	UserID    string    `json:"user_id"`
 	CreatedAt time.Time `json:"created_at"`
 	ExpiresAt time.Time `json:"expires_at"`
+}
+
+type Credentials struct {
+	Phonenumber string `json:"phonenumber" form:"phonenumber" binding:"required"`
+	Password    string `json:"password" form:"password" binding:"required"`
 }
